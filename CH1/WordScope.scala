a8dbcc0d47305c3bf630df5f5d2a9287b450771a
HaryKali <HaryKali@outlook.com>
2026-02-19 23:43:32 +0800

    lazy update

--- CH1/WordScope.scala
+++ CH1/WordScope.scala
@@ -1,9 +1,5 @@
-def increment(x: Int): Int = x + 1
-
-def getFirstChar(s: String): Char = s(0)
-
-def wordScope(word:String): Int =word.length() 
-
-println(increment(5)) // Output: 6
-println(getFirstChar("Hello")) // Output: H
-println(wordScope("Scala")) // Output: 5
+object Hello{
+    def main(args: Array[String]): Unit = {
+        println("We can do this all day")
+    }
+}
